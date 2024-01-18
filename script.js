a21bc0873258d8df85a90dd9985db71fc410a7b6
jibranjan <jibranjan77@gmail.com>
2024-01-18 20:42:30 +0530

    images updated

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Nav animation
 const insideNavEL = document.querySelector(".insideNav");
 
 const btnEl1 = document.getElementById("showSideMenu");
@@ -10,82 +11,6 @@ btnEl2.addEventListener("click", function () {
 });
 
 // How it works section:
-
-// const img0El = document.querySelector(".img0");
-
-// const assessmentEl = document.querySelector(".assessment");
-// const assessmentHeadingEl = document.querySelector(".assessmentHeading");
-// const assessmentIconEl = document.querySelector(".assessmentIcon");
-// const assessmentDescriptionEl = document.querySelector(
-//   ".assessmentDescription"
-// );
-
-// const dashboardEl = document.querySelector(".dashboard");
-// const dashboardHeadingEl = document.querySelector(".dashboardHeading");
-// const dashboardIconEl = document.querySelector(".dashboardIcon");
-// const dashboardDescriptionEl = document.querySelector(".dashboardDescription");
-
-// const questionsEl = document.querySelector(".questions");
-// const questionsHeadingEl = document.querySelector(".questionsHeading");
-// const questionsIconEl = document.querySelector(".questionsIcon");
-// const questionsDescriptionEl = document.querySelector(".questionsDescription");
-
-// const cheatingEl = document.querySelector(".cheating");
-// const cheatingHeadingEl = document.querySelector(".cheatingHeading");
-// const cheatingIconEl = document.querySelector(".cheatingIcon");
-// const cheatingDescriptionEl = document.querySelector(".cheatingDescription");
-
-// assessmentEl.addEventListener("mouseover", function () {
-//   assessmentDescriptionEl.classList.remove("hidden");
-//   assessmentHeadingEl.classList.add("text-white");
-//   assessmentIconEl.classList.add("rotate-180");
-//   // img0El.classList.add("hidden");
-// });
-// assessmentEl.addEventListener("mouseout", function () {
-//   assessmentDescriptionEl.classList.add("hidden");
-//   assessmentHeadingEl.classList.remove("text-white");
-//   assessmentIconEl.classList.remove("rotate-180");
-//   // img0El.classList.remove("hidden");
-// });
-
-// dashboardEl.addEventListener("mouseover", function () {
-//   dashboardDescriptionEl.classList.remove("hidden");
-//   dashboardHeadingEl.classList.add("text-white");
-//   dashboardIconEl.classList.add("rotate-180");
-//   // img0El.classList.add("hidden");
-// });
-// dashboardEl.addEventListener("mouseout", function () {
-//   dashboardDescriptionEl.classList.add("hidden");
-//   dashboardHeadingEl.classList.remove("text-white");
-//   dashboardIconEl.classList.remove("rotate-180");
-//   // img0El.classList.remove("hidden");
-// });
-
-// questionsEl.addEventListener("mouseover", function () {
-//   questionsDescriptionEl.classList.remove("hidden");
-//   questionsHeadingEl.classList.add("text-white");
-//   questionsIconEl.classList.add("rotate-180");
-//   // img0El.classList.add("hidden");
-// });
-// questionsEl.addEventListener("mouseout", function () {
-//   questionsDescriptionEl.classList.add("hidden");
-//   questionsHeadingEl.classList.remove("text-white");
-//   questionsIconEl.classList.remove("rotate-180");
-//   // img0El.classList.remove("hidden");
-// });
-
-// cheatingEl.addEventListener("mouseover", function () {
-//   cheatingDescriptionEl.classList.remove("hidden");
-//   cheatingHeadingEl.classList.add("text-white");
-//   cheatingIconEl.classList.add("rotate-180");
-//   // img0El.classList.add("hidden");
-// });
-// cheatingEl.addEventListener("mouseout", function () {
-//   cheatingDescriptionEl.classList.add("hidden");
-//   cheatingHeadingEl.classList.remove("text-white");
-//   cheatingIconEl.classList.remove("rotate-180");
-//   // img0El.classList.remove("hidden");
-// });
 
 let activeCardId = "howItWorksCardAssessment";
 
